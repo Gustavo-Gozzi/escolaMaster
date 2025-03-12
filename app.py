2f52360905c1b9f5c486ee8477011486e171ad07
Gustavo_Gozzi <gustavo.gozzi@aluno.faculdadeimpacta.com.br>
2025-03-12 20:19:18 -0300

    [FEAT]Dicionario &  POST Alunos

--- app.py
+++ app.py
@@ -2,14 +2,37 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+dicionario = {
+    "Alunos": [
+        {
+            "id": 1,
+            "nome": "Daniel"
+        }
+    ],
+    "Professores": [
+        {
+            "id": "pr01",
+            "nome": "Caio"
+        }
+    ]
+}
+
 @app.route('/alunos', methods=['GET'])
 def getAlunos():
-    dados = {
-        "id": 1,
-        "nome": "Mikael"
-    }
+    dados = dicionario["Alunos"]
     return jsonify(dados)
 
+@app.route('/alunos', methods=["POST"])
+def postAluno():
+    novo_aluno = {
+        "id": 2,
+        "nome": "Frodo"
+    }
+    
+    dados = dicionario["Alunos"]
+    dados.append(novo_aluno)
+    return jsonify(dados)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
