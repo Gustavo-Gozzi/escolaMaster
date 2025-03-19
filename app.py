c69c976329ca493b327dd327b53ff8eb061f7554
Gustavo-Gozzi <gozzigustavo@gmail.com>
2025-03-19 17:04:48 -0300

    [aprv] codigo aprovado

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def getAlunos():
 
 @app.route('/alunos/<int:idAluno>', methods=['GET'])
 def getAlunosbyID(idAluno):
-    alunos = dicionario["Alunos"] #-> Array JSONs
+    alunos = dicionario["Alunos"]
     for aluno in alunos:
         if aluno["id"] == idAluno:
             return jsonify(aluno)
@@ -89,19 +89,19 @@ def getAlunosbyID(idAluno):
 @app.route('/alunos', methods=["POST"])
 def postAlunos():
     if empty("Turma"):
-        return jsonify("Não há turmas criadas, impossível de registrar alunos.")
+        return jsonify("Não há turmas criadas, impossível de registrar alunos."),400
     dados = request.json
     alunos = dicionario["Alunos"]
     turmas = dicionario["Turma"]
-
-    #Verifica se já tem um aluno na
+    if not "data_nascimento" in dados:
+        return jsonify("Impossível registrar aluno sem Data de Nascimento."),400
     try:
         turma_existe = False
 
         for turma in turmas:
             if turma["id"] == dados["turma_id"]:
                 turma_existe = True
-                break  # Se encontrou a turma, sai do loop
+                break  
         
         if not turma_existe:
             raise ValueError("Impossível criar aluno sem uma turma existente")
@@ -116,13 +116,12 @@ def postAlunos():
     if "nome" not in dados:
         return jsonify({"erro": "aluno sem nome"}),400 
 
-    dt_nascimento = dados["data_nascimento"] # Retorna a data de Nascimento fornecida
-    idade = calcula_idade(dt_nascimento)     # Chama a função de Calcular Idade
-    dados["idade"] = idade                   # Adiciona a chave 'Idade' junto com o valor retornado da função
-
-    nota1 = dados["nota_primeiro_semestre"]  #Retorna a primeira nota
-    nota2 = dados["nota_segundo_semestre"]   #Retorna a segunda nota
-    dados["media_final"] = media(nota1, nota2) #Atribui a chave 'media_final' o retorno da função
+    dt_nascimento = dados["data_nascimento"] 
+    idade = calcula_idade(dt_nascimento)     
+    dados["idade"] = idade                  
+    nota1 = dados["nota_primeiro_semestre"]  
+    nota2 = dados["nota_segundo_semestre"]   
+    dados["media_final"] = media(nota1, nota2) 
 
     alunos.append(dados)
     return jsonify(dados)
@@ -138,18 +137,18 @@ def putAlunos(idAluno):
                     return jsonify({"erro": "aluno sem nome"}),400 
 
             if aluno['nome'] != resposta['nome']: 
-                aluno['nome'] = resposta['nome'] #Se for diferente muda, se não segue
+                aluno['nome'] = resposta['nome'] 
             if aluno['data_nascimento'] != resposta['data_nascimento']:
                 aluno['data_nascimento'] = resposta['data_nascimento']
-                dt_nascimento = resposta["data_nascimento"] # Retorna a data de Nascimento fornecida
-                idade = calcula_idade(dt_nascimento)     # Chama a função de Calcular Idade
-                aluno["idade"] = idade                   # Adiciona a chave 'Idade' junto com o valor retornado da função
+                dt_nascimento = resposta["data_nascimento"] 
+                idade = calcula_idade(dt_nascimento)     
+                aluno["idade"] = idade                   
             if aluno['nota_primeiro_semestre'] != resposta["nota_primeiro_semestre"] or aluno['nota_segundo_semestre'] != resposta['nota_segundo_semestre']:
                 aluno["nota_primeiro_semestre"] = resposta["nota_primeiro_semestre"]
                 aluno["nota_segundo_semestre"] = resposta["nota_segundo_semestre"]
-                nota1 = resposta["nota_primeiro_semestre"]  #Retorna a primeira nota
-                nota2 = resposta["nota_segundo_semestre"]   #Retorna a segunda nota
-                aluno["media_final"] = media(nota1, nota2) #Atribui a chave 'media_final' o retorno da função
+                nota1 = resposta["nota_primeiro_semestre"]  
+                nota2 = resposta["nota_segundo_semestre"]   
+                aluno["media_final"] = media(nota1, nota2) 
             
             return jsonify(resposta)
     return jsonify({"erro": "Aluno não encontrado"}),404
@@ -157,9 +156,9 @@ def putAlunos(idAluno):
 @app.route('/alunos/<int:idAluno>', methods=["DELETE"])
 def deleteAlunos(idAluno):
     alunos = dicionario["Alunos"]
-    for aluno in alunos:                #percorre o Array Alunos 
-        if aluno["id"] == idAluno:      #compara os IDs
-            alunos.remove(aluno)        #Remore o objeto do array
+    for aluno in alunos: 
+        if aluno["id"] == idAluno:      
+            alunos.remove(aluno)        
             return jsonify(aluno)
     else:
         return jsonify({"erro": "Aluno não encontrado"}),404
@@ -173,10 +172,10 @@ def getProfessores():
 
 @app.route('/professores/<int:idProfessor>', methods=['GET'])
 def getProfessoresByID(idProfessor):
-    professores = dicionario["Professores"] #Adiciona o Array de professores a variavel
-    for professor in professores:           #Percorre essa lista
-        if professor["id"] == idProfessor:  #Se o ID escolhido estiver entre os IDs de professores então
-            return jsonify(professor)       #Todo o objeto será retornado
+    professores = dicionario["Professores"] 
+    for professor in professores:           
+        if professor["id"] == idProfessor:  
+            return jsonify(professor)       
     else:
         return jsonify({"erro": "Professor não encontrado"}),400
 
@@ -184,8 +183,9 @@ def getProfessoresByID(idProfessor):
 def postProfessores():
     dados = request.json
     professores = dicionario["Professores"]
+    if not "data_nascimento" in dados:
+        return jsonify("Impossível registrar professor sem Data de Nascimento."),400
 
-    #Verifica se já tem um professor com o id
     for professor in professores: 
         if professor["id"] == dados["id"]:
             return jsonify({"erro": "id já utilizada"}),400 
@@ -193,9 +193,9 @@ def postProfessores():
     if "nome" not in dados:
         return jsonify({"erro": "professor sem nome"}),400 
 
-    dt_nascimento = dados["data_nascimento"] # Retorna a data de Nascimento fornecida
-    idade = calcula_idade(dt_nascimento)     # Chama a função de Calcular Idade
-    dados["idade"] = idade                   # Adiciona a chave 'Idade' junto com o valor retornado da função
+    dt_nascimento = dados["data_nascimento"] 
+    idade = calcula_idade(dt_nascimento)     
+    dados["idade"] = idade                  
 
     professores.append(dados)
     return jsonify(dados)
@@ -226,10 +226,10 @@ def putProfessores(idProfessor):
             
 @app.route('/professores/<int:idProfessor>', methods=["DELETE"])
 def deleteProfessores(idProfessor):
-    professores = dicionario["Professores"]      #Resgata a lista de dicionarios professores
-    for professor in professores:                #percorre o Array professores 
-        if professor["id"] == idProfessor:      #compara os IDs
-            professores.remove(professor)        #Remore o objeto do array
+    professores = dicionario["Professores"]      
+    for professor in professores:                
+        if professor["id"] == idProfessor:      
+            professores.remove(professor)       
             resposta = "Professor deletado com sucesso!"
             return jsonify(resposta)
     else:
@@ -264,7 +264,7 @@ def postTurmas():
         for professor in professores:
             if professor["id"] == dados["professor_id"]:
                 professor_existe = True
-                break  # Se encontrou a turma, sai do loop
+                break  
         
         if not professor_existe:
             raise ValueError("Impossível criar turma sem um professor")
@@ -299,10 +299,10 @@ def putTurma(idTurma):
 
 @app.route('/turmas/<int:idTurma>', methods=["DELETE"])
 def deleteTurma(idTurma):
-    turmas = dicionario["Turma"]      #Resgata a lista de dicionarios turmas
-    for turma in turmas:                #percorre o Array turmas 
-        if turma["id"] == idTurma:      #compara os IDs
-            turmas.remove(turma)        #Remore o objeto do array
+    turmas = dicionario["Turma"]      
+    for turma in turmas:                
+        if turma["id"] == idTurma:      
+            turmas.remove(turma)        
             resposta = "Turma deletada com sucesso!"
             return jsonify(resposta)
     else:
@@ -310,27 +310,27 @@ def deleteTurma(idTurma):
 
 #funcoes
 def calcula_idade(data):   
-    data = data.split("-")  #Transforma o dado recebido em lista usando '-' como separador
-    ano = int(data[0])      #A lisa ficará assim: ["29", "07", "2003"]
-    mes = int(data[1])      #Então cada variavel (ano, mes e dia) receberão seu valor exato de acordo
-    dia = int(data[2])      #com o index
-    data_nascimento = datetime.date(ano, mes, dia) #necessário importação da bibli 'datetime'
+    data = data.split("-")  
+    ano = int(data[0])      
+    mes = int(data[1])      
+    dia = int(data[2])      
+    data_nascimento = datetime.date(ano, mes, dia) 
     hoje = datetime.date.today()
     idade = hoje - data_nascimento
-    anos = idade.days // 365 #Pega o calculo de dias e divide por 365(1 ano)
+    anos = idade.days // 365 
     return anos
 
 def media(nota1, nota2):
     soma = nota1 + nota2
     return soma / 2
 
-def empty(texto):                       #Deve receber uma chave do nosso dicionario, ex: Turma
-    texto = texto.capitalize()          #Garante que a string recebida tenha a primeira letra maiuscula
-    if texto in dicionario:             #Verifica de a chave fornecida exite no dicionario.
-        if len(dicionario[texto]) > 0:  #Caso a chave exista, verifica se ela possui algum dado salvo, "tamanho > 0"
-            return False                #Se o tamanho da lista for maior que 0, então retorna falso "não está vazio"
+def empty(texto):                       
+    texto = texto.capitalize()          
+    if texto in dicionario:             
+        if len(dicionario[texto]) > 0:  
+            return False                
         else:                           
-            return True                 #Se for menor ou igual a zero, então a lista está vazia
+            return True                 
     else:
         return False
 
