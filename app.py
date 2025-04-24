a28fef81d6fe757539522d9dcbbeae3653aa0672
Daniel Figueiredo dos Santos <2400974@SALAS.AULAS>
2025-04-23 21:08:22 -0300

    [feat] adicionado de forma incompleta o swagger (falta documentar corretamente de acordo com a nossa aplicação)

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, jsonify, request
 from configuracao import app, db
 from aluno.control_aluno import aluno_blueprint
 from professor.control_professor import professor_blueprint
+from flask_swagger_ui import get_swaggerui_blueprint
 from turma.control_turma import turma_blueprint
 from reseta.control_reseta import reseta_blueprint
+
+SWAGGER_URL = '/swagger'
+API_DOC_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL,API_DOC_URL)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 app.register_blueprint(aluno_blueprint)
 app.register_blueprint(professor_blueprint)
